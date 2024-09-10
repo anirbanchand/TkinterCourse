9aea5b9d60394972a12dbbd4e3d69be53356688d
anirbanchand <anirbanchand3@gmail.com>
2024-09-10 15:45:19 +0530

    eight.py added

---
+++ eighth.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root=Tk()
+
+root.geometry=("1999x555")
+root.title("Pycharm")
+f1=Frame(root,bg="grey",borderwidth=6,relief=SUNKEN)
+f1.pack(side=LEFT, fill="y")
+
+l=Label(f1, text="Project Tkinter - Pycharm")
+l.pack()
+ f2= Fr
+
+root.mainloop()
