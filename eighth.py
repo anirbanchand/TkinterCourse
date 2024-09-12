215f37983e56bed3401e16775e49e24639f81769
anirbanchand <anirbanchand3@gmail.com>
2024-09-12 10:04:29 +0530

    eight.py modify

--- eighth.py
+++ eighth.py
@@ -7,8 +7,13 @@ root.title("Pycharm")
 f1=Frame(root,bg="grey",borderwidth=6,relief=SUNKEN)
 f1.pack(side=LEFT, fill="y")
 
+f2=Frame(root, bg="grey", borderwidth=8, relief=SUNKEN)
+f2.pack(side=TOP, fill="x")
+
 l=Label(f1, text="Project Tkinter - Pycharm")
+l.pack(pady=142)
+
+l=Label(f2,text="Welcome to Pycharm", font="Helvetica 16 bold", fg="red")
 l.pack()
- f2= Fr
 
 root.mainloop()
